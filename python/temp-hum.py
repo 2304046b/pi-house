d07c64f26f5aff992b8531463bb8d7ec8362aeca
NUR SABRINA BINTE ABDULLAH <2304046B@STUDENT.TP.EDU.SG>
2025-12-03 11:30:45 +0800

    Update Pubnub keys in temp-hum.py

--- python/temp-hum.py
+++ python/temp-hum.py
@@ -5,7 +5,7 @@ import sys
 from pubnub import Pubnub
 import Adafruit_DHT as dht
 
-pubnub = Pubnub(publish_key='demo', subscribe_key='demo')
+pubnub = Pubnub(publish_key='pub-c-d7031905-2761-47d5-a9b4-d15da7ff1536', subscribe_key='sub-c-c34fe3bd-da3e-4394-a72d-4dd48ce71bbe')
 channel = 'pi-house'
 
 def callback(message):
